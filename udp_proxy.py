348b3bdf4d4530834e4f20470734988c13efc734
konglx <kong.luoxing@gmail.com>
2014-12-08 21:04:09 +0800

    Use optparse instead of argperse, compatible with python 2.6 now

--- udp_proxy.py
+++ udp_proxy.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # coding: utf-8
 
-import argparse
+from optparse import OptionParser
 import socket
 import logging
 
@@ -15,37 +15,35 @@ logger.addHandler(handler)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = OptionParser()
 
-    parser.add_argument('--bind-address',
-                        help='The address to bind, use 0.0.0.0 for all ip address.',
-                        type=str)
-    parser.add_argument('--port',
-                        help='The port to listen, eg. 623.',
-                        type=int)
-    parser.add_argument('--dst-ip',
-                        help='Destination host ip, eg. 192.168.3.101.',
-                        type=str)
-    parser.add_argument('--dst-port',
-                        help='Destination host port, eg. 623.',
-                        type=int)
+    parser.add_option('--bind-address',
+                      help='The address to bind, use 0.0.0.0 for all ip address.')
+    parser.add_option('--port',
+                      help='The port to listen, eg. 623.',
+                      type=int)
+    parser.add_option('--dst-ip',
+                      help='Destination host ip, eg. 192.168.3.101.')
+    parser.add_option('--dst-port',
+                      help='Destination host port, eg. 623.',
+                      type=int)
 
     return parser.parse_args()
 
-namespace = parse_args()
+(options, args) = parse_args()
 
 
 def recv():
     sock_src = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock_dst = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    recv_addr = (namespace.bind_address, namespace.port)
-    dst_addr = (namespace.dst_ip, namespace.dst_port)
+    recv_addr = (options.bind_address, options.port)
+    dst_addr = (options.dst_ip, options.dst_port)
     sock_src.bind(recv_addr)
 
     while True:
         data, addr = sock_src.recvfrom(65565)
         if not data:
-            print('an error occured')
+            logger.error('an error occured')
             break
         logger.debug('received: {0!r} from {1}'.format(data, addr))
         sock_dst.sendto(data, dst_addr)
